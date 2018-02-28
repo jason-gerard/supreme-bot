41cbcf9b0ac1eb876d03462ed3e6da44ee99d7d7
Jason Gerard <jasongerard321@gmail.com>
2018-02-27 23:54:26 -0800

    used selenium driver to autofill shipping fields and checkboxes

--- supreme_bot.py
+++ supreme_bot.py
@@ -2,9 +2,11 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 class Product:
 
@@ -54,24 +56,7 @@ class Product:
         self.category = self.set_product_category()
         self.url = self.set_product_url()
 
-class SupremeBot:
-
-    def main():
-        initial_url = 'http://www.supremenewyork.com/previews/springsummer2018/bags/backpack'
-        color = 'Black'
-
-        # inits current product object
-        product = Product(initial_url, color)
-
-        print(product.url)
-
-        # inits selenium chrome web driver
-        driver = webdriver.Chrome()
-
-        driver.get(product.url)
-        # makes assertion that driver opened right page
-        assert product.name in driver.title
-
+    def add_product_to_cart(self, driver):
         # finds and clicks add ot cart button
         add_to_cart_btn = driver.find_element_by_xpath("//input[@value='add to cart']")
         add_to_cart_btn.click()
@@ -85,7 +70,70 @@ class SupremeBot:
             check_out_btn = driver.find_element_by_xpath("//a[@class='button checkout']")
             check_out_btn.click()
 
-        time.sleep(5000)
+class SupremeBot:
 
-    if __name__ == '__main__':
-        main()
+    def __init__(self):
+        pass
+
+    # locate each input and fill accordingly
+    def fill_checkout_info(self, driver):
+        name_input = driver.find_element_by_id('order_billing_name')
+        name_input.clear()
+        name_input.send_keys('John Doe')
+
+        email_input = driver.find_element_by_id('order_email')
+        email_input.clear()
+        email_input.send_keys('john@doe.com')
+
+        phone_number_input = driver.find_element_by_id('order_tel')
+        phone_number_input.clear()
+        phone_number_input.send_keys('206-444-4444')
+
+        address_input = driver.find_element_by_id('bo')
+        address_input.clear()
+        address_input.send_keys('400 Broad St')
+
+        zip_code_input = driver.find_element_by_id('order_billing_zip')
+        zip_code_input.clear()
+        zip_code_input.send_keys('98101')
+
+        city_input = driver.find_element_by_id('order_billing_city')
+        city_input.clear()
+        city_input.send_keys('Seattle')
+
+        state_input = Select(driver.find_element_by_id('order_billing_state'))
+        state_input.select_by_value('WA')
+
+        country_input = Select(driver.find_element_by_id('order_billing_country'))
+        country_input.select_by_value('USA')
+
+        order_terms_input = driver.find_element_by_class_name('terms').find_element_by_class_name('iCheck-helper')
+        order_terms_input.click()
+
+def main():
+    # initial testing values
+    initial_url = 'http://www.supremenewyork.com/previews/springsummer2018/bags/backpack'
+    color = 'Black'
+
+    # inits product object
+    product = Product(initial_url, color)
+    #init a supreme bot object
+    supreme_bot = SupremeBot()
+
+    # inits selenium chrome web driver
+    driver = webdriver.Chrome()
+    # gets url and opens it
+    driver.get(product.url)
+    # makes assertion that driver opened right page
+    assert product.name in driver.title
+
+    # adds product to cart
+    product.add_product_to_cart(driver)
+
+    # fills out checkout information
+    supreme_bot.fill_checkout_info(driver)
+
+    time.sleep(5000)
+
+if __name__ == '__main__':
+    main()
